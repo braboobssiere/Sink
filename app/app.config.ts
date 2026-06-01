b411924b5ba1ff21f33c7f06b9e73c15db08d10e
Boobs <86918103+braboobssiere@users.noreply.github.com>
2026-06-02 01:34:32 +0700

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
   title: 'Sink',
-  github: 'https://github.com/miantiao-me/sink',
-  coffee: 'https://sink.cool/coffee',
-  twitter: 'https://sink.cool/x',
-  telegram: 'https://sink.cool/telegram',
+  github: 'https://github.com/',
+  coffee: 'https://en.wikipedia.org/wiki/Coffee',
+  twitter: 'https://x.com/',
+  telegram: 'https://t.me/',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  image: 'https://raw.githubusercontent.com/braboobssiere/Sink/refs/heads/master/public/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
